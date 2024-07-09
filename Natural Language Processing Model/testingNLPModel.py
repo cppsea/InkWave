04a9602f0b416d4993de6075b0f2e2baf2ed0a40
Nhan Thai <72413541+NhanThai0501@users.noreply.github.com>
2024-07-09 09:16:33 -0700

    delete unnecessary lines

--- Natural Language Processing Model/testingNLPModel.py
+++ Natural Language Processing Model/testingNLPModel.py
@@ -1,19 +1,3 @@
-# import NLPModel_3
-
-# # Read the text content from a file
-# LLM_to_NLP = NLPModel_3.read_file("LLM_preformatted.txt")
-
-# # Use the nlp_format function from the NLPModel_3 module with the text content
-# NLPModel_3.nlp_format(LLM_to_NLP)
-
-# import NLPModel_4
-
-# # Read the text content from a file
-# LLM_to_NLP = NLPModel_4.read_file("LLM_preformatted.txt")
-
-# # Use the nlp_format function from the NLPModel_3 module with the text content
-# NLPModel_4.nlp_format(LLM_to_NLP)
-
 from NLPModel_4 import NLPProcessor
 
 processor = NLPProcessor()
