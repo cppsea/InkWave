db56a7e1ed40144d6ef015206b5e450bb7ae72d1
Manson <mphamLQ@gmail.com>
2024-11-18 19:34:47 -0800

    start the server after connecting to db cluster

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const userRoutes = require("./routes/user.routes");
 const noteRoutes = require("./routes/note.routes");
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGO_DB;
 
 const app = express();
 
@@ -13,3 +16,14 @@ app.use("/api/user", userRoutes);
 app.use("/api/notes", noteRoutes);
 
 // ** TO DO: Start the server here (below) **
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    // if db connection is successful, then start the server
+    app.listen(process.env.PORT || 3000);
+    console.log(`Server is running on http://localhost:3000`);
+  })
+  .catch((err) => {
+    console.log("Connection to DB Cluster failed.");
+    console.log(err);
+  });
