9c1c51ee0c7f897a68135efe515d911c1fb496b7
PattyCakesxD <nguyenpatty42@gmail.com>
2024-12-06 20:42:49 -0800

    adds template for notes routes

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -2,4 +2,49 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
+/**
+ * Retrieves all notes for a user
+ * @route GET /api/notes/:id
+ */
+router.get("/:id", async (req: Request, res: Response) => {
+  res.send("GET request to the homepage");
+  // ** TO DO ** //
+});
+
+/**
+ * Retrieves a specific note for a user
+ * @route GET /api/notes/:id/:noteId
+ */
+router.get("/:id/:noteId", async (req: Request, res: Response) => {
+  res.send("GET request to the homepage");
+  // ** TO DO ** //
+});
+
+/**
+ * Receive an image and sends it to the ML models
+ * @route POST /api/notes/summary
+ */
+router.post("/summary", async (req: Request, res: Response) => {
+  res.send("POST request to the homepage");
+  // ** TO DO ** //
+});
+
+/**
+ * Handles saving a note to the database (editing purposes)
+ * @route PUT /api/notes/save/:id
+ */
+router.put("/save/:id", async (req: Request, res: Response) => {
+  res.send("PUT request to the homepage");
+  // ** TO DO ** //
+});
+
+/**
+ * Handles deleting a note from the database
+ * @route DELETE /api/notes/delete/:id
+ */
+router.delete("/delete/:id", async (req: Request, res: Response) => {
+  res.send("DELETE request to the homepage");
+  // ** TO DO ** //
+});
+
 export default router;
