fd292df3d14f10b55c8e76ec1621a38643a3919c
callistabudiman16 <callistabudiman@yahoo.com>
2025-02-18 15:36:14 -0800

    Handle open recent

--- Frontend/src/renderer/src/pages/Home/Home.jsx
+++ Frontend/src/renderer/src/pages/Home/Home.jsx
@@ -17,25 +17,25 @@ const Home = ({userID, setUserID}) => {
     setShowPopup(false)
   }
 
-  // const handleOpenRecent = async () => {
-  //   try{
-  //     const response = await fetch('http://localhost:1400/api/notes/recent/${67937b5f5d69699fa872f96e}', {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type' : 'application/json',
-  //       }
-  //     });
-  //     if(!response.ok) {
-  //       throw new Error(`Error opening file Stats: ${response.status}`);
+  const handleOpenRecent = async () => {
+    try{
+      const response = await fetch('http://localhost:1400/api/notes/recent/${67937b5f5d69699fa872f96e}', {
+        method: 'GET',
+        headers: {
+          'Content-Type' : 'application/json',
+        }
+      });
+      if(!response.ok) {
+        throw new Error(`Error opening file Stats: ${response.status}`);
 
-  //     }
+      }
       
-  //     const data = await response.json();
-  //     console.log('fetched recent files: ', data);
-  //   } catch(error){
-  //     console.error('Error fetching recent files', error);
-  //   }
-  // }
+      const data = await response.json();
+      console.log('fetched recent files: ', data);
+    } catch(error){
+      console.error('Error fetching recent files', error);
+    }
+  }
 
   return (
     <div className="home-container">
